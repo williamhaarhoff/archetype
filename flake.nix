08189cc4c2bbed313e378103d309a486263cba72
Will <williamhaarhoff@gmail.com>
2025-06-06 06:29:18 +1200

    tests

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # The `callPackage` automatically fills the parameters of the function
+        # in package.nix with what's inside the `pkgs` attribute.
+        packages.default = pkgs.callPackage ./package.nix { };
+        packages.clang = pkgs.callPackage ./package.nix { stdenv = pkgs.clangStdenv; };
+
+        # The `config` variable contains our own outputs, so we can reference
+        # neighbor attributes like the package we just defined one line earlier.
+        devShells.default = config.packages.default;
+      };
+    };
+}
---
+++ package.nix
@@ -0,0 +1,38 @@
+# file: package.nix
+
+# The items in the curly brackets are function parameters as this is a Nix
+# function that accepts dependency inputs and returns a new package
+# description
+{ lib
+, stdenv
+, cmake
+, boost
+, catch2
+}:
+
+# stdenv.mkDerivation now accepts a list of named parameters that describe
+# the package itself.
+
+stdenv.mkDerivation {
+  name = "cpp-nix";
+
+  # good source filtering is important for caching of builds.
+  # It's easier when subprojects have their own distinct subfolders.
+  src = lib.sourceByRegex ./. [
+    "^src.*"
+    "^test.*"
+    "CMakeLists.txt"
+  ];
+
+  # We now list the dependencies similar to the devShell before.
+  # Distinguishing between `nativeBuildInputs` (runnable on the host
+  # at compile time) and normal `buildInputs` (runnable on target
+  # platform at run time) is an important preparation for cross-compilation.
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ boost catch2 ];
+
+  # Instruct the build process to run tests.
+  # The generic builder script of `mkDerivation` handles all the default
+  # command lines of several build systems, so it knows how to run our tests.
+  doCheck = true;
+}
