df76c4e284c26d1a50429f296cace9172d44bfc0
Will <williamhaarhoff@gmail.com>
2025-06-11 17:23:19 +1200

    Base ptr templated on interfaces

--- include/archetype/archetype.h
+++ include/archetype/archetype.h
@@ -1,7 +1,7 @@
 #ifndef __ARCHETYPE_H__
 #define __ARCHETYPE_H__
 
-typedef int archetype;
+
 
 
 #endif //__ARCHETYPE_H__
--- src/example.cpp
+++ src/example.cpp
@@ -42,7 +42,6 @@ class ReadInterface : public R
   int read_api(char * buf, size_t size) { return this->read(buf, size); }
 };
 
-
 // Composed interfaces
 template<ReadWrite RW>
 class ReadWriteInterface : public ReadInterface<WriteInterface<RW>>
@@ -65,17 +64,16 @@ class Base
   void * _obj;
 };
 
-
 template<typename T=Base>
 class WritableBase : public T
-{
-  using T::_obj;
+{ 
   public:
   int write(const char * buf, size_t size)
   {
     return _write_callstub(_obj, buf, size);
   }
 
+  public:
   template<Writable W>
   void bind(W & w)
   {
@@ -88,49 +86,33 @@ class WritableBase : public T
 
   protected:
   int(*_write_callstub)(void*, const char *, size_t);
+  using T::_obj;
 };
 
+template<typename T=Base>
+class ReadableBase : public T
+{
+  public:
+  int read(char * buf, size_t size)
+  {
+    return _read_callstub(_obj, buf, size);
+  }
 
+  public:
+  template<Readable R>
+  void bind(R & r)
+  {
+    this->T::bind(r); // call immediate parent - which chains all the way down
+    _read_callstub = [](void * obj, char * buf, size_t size) -> int
+    {
+      return static_cast<R*>(obj)->read(buf, size);
+    };
+  }
 
-
-// template<typename T=Base>
-// class ReadableBase : public T 
-// {
-//   public:
-//   virtual int read(char *buf, size_t size) = 0;
-// };
-
-
-// Adaptors
-// template <Writable T>
-// class WritableAdaptor : public WritableBase<> {
-//   public:
-//   WritableAdaptor(T *ptr) : impl(ptr) {}
-//   virtual size_t write(const char *buf, size_t size) override { return impl->write(buf, size); }
-//   private:
-//   T *impl;
-// };
-
-
-// template <Readable T>
-// class ReadableAdaptor : public ReadableBase<> {
-//   public:
-//   ReadableAdaptor(T *ptr) : impl(ptr) {}
-//   virtual int read(char *buf, size_t size) override { return impl->read(buf, size); }
-//   private:
-//   T *impl;
-// };
-
-
-// template <ReadWrite T>
-// class ReadWriteAdaptor : public ReadableBase<WritableBase<>> {
-//   public:
-//   ReadWriteAdaptor(T *ptr) : impl(ptr) {}
-//   virtual size_t write(const char *buf, size_t size) override { return impl->write(buf, size); }
-//   virtual int read(char *buf, size_t size) override { return impl->read(buf, size); }
-//   private:
-//   T *impl;
-// };
+  protected:
+  using T::_obj;
+  int(*_read_callstub)(void*, char *, size_t);
+};
 
 
 // These are implementations provided by an external library
@@ -180,63 +162,6 @@ private:
 };
 
 
-
-// struct WritableArchetype
-// {
-//   using base = WritableBase<>;
-
-//   template <Writable W>
-//   using adaptor = WritableAdaptor<W>;
-// };
-
-
-// struct ReadableArchetype
-// {
-//   using base = ReadableBase<>;
-
-//   template <Readable R>
-//   using adaptor = ReadableAdaptor<R>;
-// };
-
-
-// struct ReadWriteArchetype
-// {
-//   using base = ReadableBase<WritableBase<>>;
-
-//   template <ReadWrite RW>
-//   using adaptor = ReadWriteAdaptor<RW>;
-// };
-
-
-// class ReadWriteDependentClass
-// {
-//   public:
-
-//   template <ReadWrite RW>
-//   void set_port(RW * target_port)
-//   {
-//     static ReadWriteArchetype::adaptor<RW> port_adaptor(target_port);
-//     port = static_cast<ReadWriteInterface<ReadWriteArchetype::base>*>(static_cast<ReadWriteArchetype::base*>(&port_adaptor));
-//   }
-
-//   ReadWriteInterface<ReadWriteArchetype::base>* port = nullptr;
-// };
-
-
-// class WriteDependentClass2
-// {
-//   public:
-
-//   template <Writable W>
-//   void set_port(W * target_port)
-//   {
-//     static WritableArchetype::adaptor<W> port_adaptor(target_port);
-//     port = static_cast<WriteInterface<WritableArchetype::base>*>(static_cast<WritableArchetype::base*>(&port_adaptor));
-//   }
-
-//   WriteInterface<WritableArchetype::base> * port = nullptr;
-// };
-
 class WriteDependentClass
 {
   public:
@@ -261,9 +186,59 @@ class ReadWriteDependentClass
     port.bind(target_port);
   }
 
-  WriteInterface<ReadableBase<WritableBase<>>> port;
+  ReadWriteInterface<ReadableBase<WritableBase<>>> port;
 };
 
+
+// struct ReadWriteArchetype
+// {
+  
+//   struct ptr
+//   {
+//     private:
+//     using base = ReadableBase<WritableBase<>>;
+//     base b;
+
+//     public:
+//     base* operator->() {return &b; }
+//     base& operator*() { return *operator->(); }
+//   };
+
+// };
+
+
+
+// ARCHETYPE_DEFINE()
+// ARCHETYPE_COMPOSE()
+
+// struct UseA
+// {
+//   template<A::compatible(T)>
+//   void set_port(T & t) { base_port.bind(t); }
+//   A::ptr a;
+// };
+
+
+
+
+template<template<typename> class Interface>
+struct rw_ptr
+{
+  private:
+  using base = Interface<ReadableBase<WritableBase<>>>;
+  base impl;
+
+  public:
+
+  template<ReadWrite RW>
+  void bind(RW & ref) { impl.bind(ref); }
+
+  base& operator*() { return &impl;}
+  const base& operator*() const { return &impl;}
+
+  base* operator->() { return &impl; }
+  const base* operator->() const { return &impl; }
+};
 
 
 
@@ -273,37 +248,43 @@ class ReadWriteDependentClass
 
 int main() 
 {
-  Writer nrw;
+  Writer nw;
   ComposedReadWriter crw;
 
-  WriteDependentClass wdc;
-  // ReadWriteDependentClass rwdc;
+  // static casting
+  WriteInterface<Writer> * write_iface = static_cast<WriteInterface<Writer>*>(&nw);
+  write_iface->write_api("hello from writer with extended write api\n", 42);
+
+  // augmentation
+  WriteInterface<Writer> augmented_writer;
+  augmented_writer.write_api("hello from augmented writer with write api\n", 43);
+
+  // generic concept ptr
+  rw_ptr<ReadWriteInterface> ptr;
+  ptr.bind(crw);
+  ptr->write_api("hello from generic concept ptr!\n", 32);
 
 
-  // ErasedWritable ew;
-  // ew.bind(crw);
-  // ew.write("hello from erased interface!", 28);
 
 
-  wdc.set_port(nrw);
-  wdc.port.write_api("hello from writer", 17);
 
+  // wdc.set_port(nw);
+  // wdc.port.write_api("hello from writer", 17);
   // wdc.set_port(crw);
-  // wdc.port->write_api("hello from composed read writer!", 32);
+  // wdc.port.write_api("hello from writer", 17);
 
   // rwdc.set_port(crw);
+  
+  
 
 
 
-
-
-  // char buf[4096];
-  // int read = 0;
-  // while(1)
-  // {
-    
-  //   read = rwdc.port->read_api(buf, sizeof(buf));
-  //   rwdc.port->write_api(buf, read);
-  // }
+  char buf[4096];
+  int read = 0;
+  while(1)
+  {
+    read = ptr->read_api(buf, sizeof(buf));
+    ptr->write_api(buf, read);
+  }
 
 }
