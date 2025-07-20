84dae2c5f8fa2cbec913f7bd512ee2df35d17909
Will <williamhaarhoff@gmail.com>
2025-07-21 07:26:58 +1200

    Better pattern

--- src/patterns.cpp
+++ src/patterns.cpp
@@ -2,27 +2,55 @@
 
 #include <cstddef>
 #include <iostream>
-struct writable
-{
-  struct vtable
-  {
-    int (*write)(void * obj, const char *, int);
-  };
 
-  template <typename T>
-  static vtable * make_vtable()
-  {
-    static vtable vtablet;
-    vtablet.write = [](void * obj, const char * arg0, int arg1) -> int {
-      return static_cast<T*>(obj)->write(arg0, arg1);
-    };
+
+struct DummyVTable {
+
+  template<typename T>
+  static DummyVTable * make_vtable() {
+    static DummyVTable vtablet;
     return &vtablet;
   }
+
+  template <typename T>
+  void bind() {}
+  
+};
+
+
+
+struct writable
+{
+  template<typename BaseVTable = DummyVTable>
+  struct vtable : public BaseVTable
+  {
+    int (*write)(void * obj, const char *, int);
+    
+    template<typename T>
+    void bind()
+    {
+      this->BaseVTable::template bind<T>();
+
+      write = [](void * obj, const char * arg0, int arg1) -> int {
+        return static_cast<T*>(obj)->write(arg0, arg1); 
+      };
+    }
+
+    public:
+    template<typename T>
+    static vtable * make_vtable()
+    {
+      static vtable<BaseVTable> vtablet;
+      vtablet.bind<T>();
+
+      return &vtablet;
+    }
+  };
 
   struct view
   {
     void * obj;
-    vtable * vtbl;
+    vtable<> * vtbl;
     int write(const char * arg0, int arg1) { return vtbl->write(obj, arg0, arg1); }
   };
 
@@ -31,28 +59,145 @@ struct writable
   {
     view v;
     v.obj = static_cast<void *>(&t);
-    v.vtbl = make_vtable<T>();
+    v.vtbl = vtable<>::make_vtable<T>();
     return v;
   }
 };
 
 
-class Writer {
-public:
-  int write(const char *buf, size_t size) {
-    int n = 0;
-    while (size--) { std::cout << buf[n++]; }
-    std::cout << std::flush;
-    return n;
+struct readable
+{
+  template<typename BaseVTable = DummyVTable>
+  struct vtable : public BaseVTable
+  {
+    int (*read)(void * obj, const char *, int);
+    
+    template<typename T>
+    void bind()
+    {
+      this->BaseVTable::template bind<T>();
+
+      read = [](void * obj, const char * arg0, int arg1) -> int {
+        return static_cast<T*>(obj)->read(arg0, arg1); 
+      };
+    }
+
+    public:
+    template<typename T>
+    static vtable * make_vtable()
+    {
+      static vtable<BaseVTable> vtablet;
+      vtablet.bind<T>();
+
+      return &vtablet;
+    }
+  };
+
+  struct view
+  {
+    void * obj;
+    vtable<> * vtbl;
+    int read(const char * arg0, int arg1) { return vtbl->read(obj, arg0, arg1); }
+  };
+
+  template<typename T>
+  static view make_view(T & t)
+  {
+    view v;
+    v.obj = static_cast<void *>(&t);
+    v.vtbl = vtable<>::make_vtable<T>();
+    return v;
+  }
+};
+
+struct readwritable
+{
+  template<typename BaseVTable = DummyVTable>
+  struct vtable : public BaseVTable
+  {
+    int (*read)(void * obj, const char *, int);
+    
+    template<typename T>
+    void bind()
+    {
+      this->BaseVTable::template bind<T>();
+    }
+
+    public:
+    template<typename T>
+    static vtable * make_vtable()
+    {
+      static vtable<BaseVTable> vtablet;
+      vtablet.bind<T>();
+
+      return &vtablet;
+    }
+  };
+
+  struct view
+  {
+    void * obj;
+    vtable<> * vtbl;
+    int read(const char * arg0, int arg1) { return vtbl->read(obj, arg0, arg1); }
+  };
+
+  template<typename T>
+  static view make_view(T & t)
+  {
+    view v;
+    v.obj = static_cast<void *>(&t);
+    v.vtbl = vtable<>::make_vtable<T>();
+    return v;
   }
 };
 
 
 
+
+
+
+struct AbstractWriter
+{
+  virtual int write(const char *buf, size_t size) = 0;
+};
+
+struct AbstractReader
+{
+  virtual int read(char *buf, size_t size) = 0;
+};
+
+struct DerivedWriter : public AbstractWriter
+{
+  virtual int write(const char *buf, size_t size) final {
+    int n = 0;
+    while (size--) {
+      std::cout << buf[n++];
+    }
+    std::cout << std::flush;
+    return n;
+  }
+};
+
+struct DerivedReadWriter : public AbstractWriter, public AbstractReader
+{
+  virtual int write(const char *buf, size_t size) final {
+    int n = 0;
+    while (size--) {
+      std::cout << buf[n++];
+    }
+    std::cout << std::flush;
+    return n;
+  }
+  virtual int read(char *buf, size_t size) final {
+    return std::fread(buf, 1, size, stdin);
+  }
+};
+
+
 int main()
 {
-  Writer w;
-  writable::view v = writable::make_view(w);
+  DerivedReadWriter wr;
+  writable::view v = writable::make_view(wr);
 
   v.write("hello\n", 7);
   return 0;
